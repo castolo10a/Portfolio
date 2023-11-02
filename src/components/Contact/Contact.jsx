68b0369d9f2baaeddc6082c8db26e749341c41d7
Daniel Castillo <Daniel.302009@hotmail.com>
2023-11-02 16:55:17 -0500

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -77,8 +77,8 @@ export default function Contact () {
         })
         emailjs
         .sendForm(
-          process.env.service_194kj3g,
-          process.env.contact_form,
+          process.env.REACT_APP_EMAILJS_SERVICE_ID,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
           form.current,
           process.env.REACT_APP_EMAILJS_PUBLIC_KEY
         )
