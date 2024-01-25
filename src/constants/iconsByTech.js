459de7d9732a5457a87806bc7de50955b96bae37
Daniel Castillo <Daniel.302009@hotmail.com>
2024-01-25 15:49:11 -0500

    update

--- src/constants/iconsByTech.js
+++ src/constants/iconsByTech.js
@@ -19,7 +19,7 @@ export const ICONS_BY_TECH = [
     icon: <SiRedux />
   },
   {
-    name: 'Node',
+    name: 'NodeJs',
     icon: <SiNodedotjs />
   },
   {
@@ -35,7 +35,7 @@ export const ICONS_BY_TECH = [
     icon: <SiSequelize />
   },
   {
-    name: 'Tailwindcss',
+    name: 'TailwindCSS',
     icon: <SiTailwindcss />
   },
   {
