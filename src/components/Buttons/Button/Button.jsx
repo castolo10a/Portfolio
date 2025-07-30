3cc1c376b9bfe6dcb241f393522500a457976645
castolo10a <castillod159@gmail.com>
2025-07-30 17:43:37 -0500

    refactor: replace anchor tag with Link from react-scroll in Button component

--- src/components/Buttons/Button/Button.jsx
+++ src/components/Buttons/Button/Button.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import { Link } from "react-scroll";
 
 const Button = ({ path, text }) => {
-
   return (
-    <a href={path}>
-        <button className="px-3 py-1 text-base text-gray-200 bg-transparent border border-solid rounded-xl transition-colors hover:bg-gray-500/75 md:w-auto md:mb-0">
-                {text}
-        </button>
-    </a>
+    <Link to={path} smooth={true} duration={500}>
+      <button className="px-3 py-1 text-base text-gray-200 bg-transparent border border-solid rounded-xl transition-colors hover:bg-gray-500/75 md:w-auto md:mb-0">
+        {text}
+      </button>
+    </Link>
   );
 };
 
