33e7a064af4ada533838d5049e006e4e14d91549
castolo10a <castillod159@gmail.com>
2025-07-30 22:59:25 -0500

    feat: create custom hook using react-hook-form for validation and EmailJS integration

---
+++ src/hooks/useContactForm.jsx
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
+
+export const useContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    trigger,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onBlur" });
+
+  const onSubmit = (data) => {
+    toast.promise(
+      emailjs.send(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        {
+          user_name: data.name,
+          user_email: data.email,
+          user_message: data.message,
+        },
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      ),
+      {
+        loading: "Enviando mensaje...",
+        success: "¡Mensaje enviado correctamente!",
+        error: "Error al enviar el mensaje. Intenta más tarde.",
+      }
+    );
+
+    reset();
+  };
+
+  return {
+    register,
+    handleSubmit,
+    errors,
+    isValid,
+    trigger,
+    onSubmit,
+  };
+};
